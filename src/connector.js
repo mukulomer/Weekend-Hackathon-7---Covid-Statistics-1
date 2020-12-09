0efedcbec3596fdccdf03b8fdc8ee8f6c63cecfc
vaishaliravasoambi <65502403+vaishaliravasoambi@users.noreply.github.com>
2020-12-09 08:26:36 +0530

    change

--- src/connector.js
+++ src/connector.js
@@ -1,18 +1,16 @@
+const mongoURI = "mongodb://localhost:27017" + "/covidtally";
 
-//const mongodb = require('mongodb');
+let mongoose = require("mongoose");
+const { tallySchema } = require("./schema");
 
-const mongoURI = "mongodb://localhost:27017" + "/covidtally"
-
-let mongoose = require('mongoose');
-const { tallySchema } = require('./schema')
-
-
-mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => { console.log("connection established with mongodb server online"); })
-    .catch(err => {
-        console.log("error while connection", err)
-    });
-collection_connection = mongoose.model('covidtally', tallySchema)
-
+mongoose
+  .connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("connection established with mongodb server online");
+  })
+  .catch((err) => {
+    console.log("error while connection", err);
+  });
+collection_connection = mongoose.model("covidtally", tallySchema);
 
 exports.connection = collection_connection;
